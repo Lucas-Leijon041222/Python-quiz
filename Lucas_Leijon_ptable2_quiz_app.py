3a69958d8e7b782f0685dfef98d1f7c0a1f7ce94
Lucas Leijon <112874076+Lucas-Leijon041222@users.noreply.github.com>
2024-04-09 13:40:20 +0200

    Ptable quiz 2

---
+++ Lucas_Leijon_ptable2_quiz_app.py
@@ -0,0 +1,623 @@
+questions = [
+   {
+        "question": "What element is identified by atomic number 4?",
+        "options": [
+            "A: H",
+            "B: He",
+            "C: Li",
+            "D: Be"
+        ],
+        "answer": "D"
+    },
+    {
+        "question": "What element is identified by atomic number 2?",
+        "options": [
+            "A: H",
+            "B: He",
+            "C: Li",
+            "D: Be"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What element is identified by atomic number 3?",
+        "options": [
+            "A: H",
+            "B: He",
+            "C: Li",
+            "D: Be"
+        ],
+        "answer": "C"
+    },
+    {
+        "question": "What element is identified by atomic number 1?",
+        "options": [
+            "A: H",
+            "B: He",
+            "C: Li",
+            "D: Be"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 118?",
+        "options": [
+            "A: Og",
+            "B: Fl",
+            "C: Lv",
+            "D: Ts"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 115?",
+        "options": [
+            "A: Ts",
+            "B: Lv",
+            "C: Fl",
+            "D: Og"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What element is identified by atomic number 117?",
+        "options": [
+            "A: Lv",
+            "B: Fl",
+            "C: Ts",
+            "D: Og"
+        ],
+        "answer": "C"
+    },
+    {
+        "question": "What element is identified by atomic number 116?",
+        "options": [
+            "A: Fl",
+            "B: Lv",
+            "C: Og",
+            "D: Ts"
+        ],
+        "answer": "D"
+    },
+    {
+        "question": "What element is identified by atomic number 114?",
+        "options": [
+            "A: Fl",
+            "B: Lv",
+            "C: Og",
+            "D: Ts"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 113?",
+        "options": [
+            "A: Ts",
+            "B: Og",
+            "C: Fl",
+            "D: Lv"
+        ],
+        "answer": "D"
+    },
+    {
+        "question": "What element is identified by atomic number 112?",
+        "options": [
+            "A: Lv",
+            "B: Ts",
+            "C: Fl",
+            "D: Og"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 111?",
+        "options": [
+            "A: Fl",
+            "B: Ts",
+            "C: Og",
+            "D: Lv"
+        ],
+        "answer": "C"
+    },
+    {
+        "question": "What element is identified by atomic number 110?",
+        "options": [
+            "A: Mt",
+            "B: Ds",
+            "C: Rg",
+            "D: Cn"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 109?",
+        "options": [
+            "A: Rg",
+            "B: Cn",
+            "C: Nh",
+            "D: Fl"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What element is identified by atomic number 108?",
+        "options": [
+            "A: Ds",
+            "B: Mt",
+            "C: Cn",
+            "D: Rg"
+        ],
+        "answer": "C"
+    },
+    {
+        "question": "What element is identified by atomic number 107?",
+        "options": [
+            "A: Cn",
+            "B: Nh",
+            "C: Fl",
+            "D: Ds"
+        ],
+        "answer": "D"
+    },
+    {
+        "question": "What element is identified by atomic number 106?",
+        "options": [
+            "A: Sg",
+            "B: Bh",
+            "C: Hs",
+            "D: Mt"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 105?",
+        "options": [
+            "A: Bh",
+            "B: Sg",
+            "C: Mt",
+            "D: Hs"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What element is identified by atomic number 104?",
+        "options": [
+            "A: Mt",
+            "B: Hs",
+            "C: Sg",
+            "D: Bh"
+        ],
+        "answer": "C"
+    },
+    {
+        "question": "What element is identified by atomic number 103?",
+        "options": [
+            "A: Hs",
+            "B: Mt",
+            "C: Sg",
+            "D: Bh"
+        ],
+        "answer": "D"
+    },
+       {
+        "question": "What element is identified by atomic number 102?",
+        "options": [
+            "A: No",
+            "B: Lr",
+            "C: Md",
+            "D: Rf"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 101?",
+        "options": [
+            "A: Lr",
+            "B: Md",
+            "C: No",
+            "D: Rf"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What element is identified by atomic number 100?",
+        "options": [
+            "A: Md",
+            "B: No",
+            "C: Lr",
+            "D: Rf"
+        ],
+        "answer": "C"
+    },
+    {
+        "question": "What element is identified by atomic number 99?",
+        "options": [
+            "A: Rf",
+            "B: No",
+            "C: Lr",
+            "D: Md"
+        ],
+        "answer": "D"
+    },
+    {
+        "question": "What element is identified by atomic number 98?",
+        "options": [
+            "A: Cf",
+            "B: Es",
+            "C: Fm",
+            "D: Md"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 97?",
+        "options": [
+            "A: Es",
+            "B: Cf",
+            "C: Fm",
+            "D: Md"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What element is identified by atomic number 96?",
+        "options": [
+            "A: Fm",
+            "B: Es",
+            "C: Cf",
+            "D: Md"
+        ],
+        "answer": "C"
+    },
+    {
+        "question": "What element is identified by atomic number 95?",
+        "options": [
+            "A: Es",
+            "B: Cf",
+            "C: Fm",
+            "D: Md"
+        ],
+        "answer": "D"
+    },
+      {
+        "question": "What element is identified by atomic number 94?",
+        "options": [
+            "A: Am",
+            "B: Cm",
+            "C: Bk",
+            "D: Cf"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 93?",
+        "options": [
+            "A: Cm",
+            "B: Am",
+            "C: Bk",
+            "D: Cf"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What element is identified by atomic number 92?",
+        "options": [
+            "A: Bk",
+            "B: Am",
+            "C: Cf",
+            "D: Cm"
+        ],
+        "answer": "C"
+    },
+    {
+        "question": "What element is identified by atomic number 91?",
+        "options": [
+            "A: Am",
+            "B: Cm",
+            "C: Bk",
+            "D: Cf"
+        ],
+        "answer": "D"
+    },
+     {
+        "question": "What element is identified by atomic number 90?",
+        "options": [
+            "A: Th",
+            "B: U",
+            "C: Np",
+            "D: Pu"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 89?",
+        "options": [
+            "A: U",
+            "B: Th",
+            "C: Np",
+            "D: Pu"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What element is identified by atomic number 88?",
+        "options": [
+            "A: Np",
+            "B: Th",
+            "C: U",
+            "D: Pu"
+        ],
+        "answer": "C"
+    },
+    {
+        "question": "What element is identified by atomic number 87?",
+        "options": [
+            "A: Th",
+            "B: U",
+            "C: Np",
+            "D: Fr"
+        ],
+        "answer": "D"
+    },
+    {
+        "question": "What element is identified by atomic number 86?",
+        "options": [
+            "A: Rn",
+            "B: Fr",
+            "C: Ra",
+            "D: Ac"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 85?",
+        "options": [
+            "A: Fr",
+            "B: Ra",
+            "C: Rn",
+            "D: Ac"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What element is identified by atomic number 84?",
+        "options": [
+            "A: Rn",
+            "B: Ra",
+            "C: Fr",
+            "D: Po"
+        ],
+        "answer": "D"
+    },
+    {
+        "question": "What element is identified by atomic number 83?",
+        "options": [
+            "A: Ra",
+            "B: Rn",
+            "C: Fr",
+            "D: Po"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 82?",
+        "options": [
+            "A: Pb",
+            "B: Bi",
+            "C: Po",
+            "D: Tl"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 81?",
+        "options": [
+            "A: Bi",
+            "B: Pb",
+            "C: Tl",
+            "D: Po"
+        ],
+        "answer": "C"
+    },
+    {
+        "question": "What element is identified by atomic number 80?",
+        "options": [
+            "A: Pb",
+            "B: Bi",
+            "C: Po",
+            "D: Tl"
+        ],
+        "answer": "D"
+    },
+    {
+        "question": "What element is identified by atomic number 79?",
+        "options": [
+            "A: Au",
+            "B: Hg",
+            "C: Pb",
+            "D: Tl"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 78?",
+        "options": [
+            "A: Pt",
+            "B: Au",
+            "C: Hg",
+            "D: Ir"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What element is identified by atomic number 77?",
+        "options": [
+            "A: Ir",
+            "B: Au",
+            "C: Pt",
+            "D: Hg"
+        ],
+        "answer": "C"
+    },
+    {
+        "question": "What element is identified by atomic number 76?",
+        "options": [
+            "A: Os",
+            "B: Pt",
+            "C: Hg",
+            "D: Ir"
+        ],
+        "answer": "D"
+    },
+    {
+        "question": "What element is identified by atomic number 75?",
+        "options": [
+            "A: Re",
+            "B: Os",
+            "C: Ta",
+            "D: W"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 74?",
+        "options": [
+            "A: W",
+            "B: Re",
+            "C: Os",
+            "D: Ta"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What element is identified by atomic number 73?",
+        "options": [
+            "A: Ta",
+            "B: W",
+            "C: Re",
+            "D: Os"
+        ],
+        "answer": "C"
+    },
+    {
+        "question": "What element is identified by atomic number 72?",
+        "options": [
+            "A: Hf",
+            "B: Ta",
+            "C: Re",
+            "D: W"
+        ],
+        "answer": "D"
+    },
+    {
+        "question": "What element is identified by atomic number 71?",
+        "options": [
+            "A: Lu",
+            "B: Hf",
+            "C: Ta",
+            "D: W"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 70?",
+        "options": [
+            "A: Yb",
+            "B: Lu",
+            "C: Hf",
+            "D: Ta"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What element is identified by atomic number 69?",
+        "options": [
+            "A: Hf",
+            "B: Lu",
+            "C: Yb",
+            "D: Tm"
+        ],
+        "answer": "C"
+    },
+    {
+        "question": "What element is identified by atomic number 68?",
+        "options": [
+            "A: Ho",
+            "B: Tm",
+            "C: Yb",
+            "D: Lu"
+        ],
+        "answer": "D"
+    },
+    {
+        "question": "What element is identified by atomic number 67?",
+        "options": [
+            "A: Tm",
+            "B: Ho",
+            "C: Er",
+            "D: Yb"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What element is identified by atomic number 66?",
+        "options": [
+            "A: Ho",
+            "B: Er",
+            "C: Tm",
+            "D: Dy"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What element is identified by atomic number 65?",
+        "options": [
+            "A: Dy",
+            "B: Ho",
+            "C: Er",
+            "D: Tm"
+        ],
+        "answer": "C"
+    },
+    {
+        "question": "What element is identified by atomic number 64?",
+        "options": [
+            "A: Er",
+            "B: Dy",
+            "C: Ho",
+            "D: Tm"
+        ],
+        "answer": "D"
+    },
+    {
+        "question": "What element is identified by atomic number 63?",
+        "options": [
+            "A: Dy",
+            "B: Ho",
+            "C: Er",
+            "D: Tm"
+        ],
+        "answer": "A"
+    },
+]
+
+
+def run_quiz(questions):
+    score = 0  
+
+    for question in questions:
+        print("\n" + question["question"])  
+        for option in question["options"]:
+            print(option)
+
+        user_answer = input("Enter your answer (A, B, C, or D): ").strip().upper()
+
+        if user_answer == question["answer"]:
+            print("Correct!")
+            score += 1  
+        else:
+            print("Wrong answer.")
+
+    print(f"\nYou got {score} out of {len(questions)} questions correct!")
+
+run_quiz(questions)
